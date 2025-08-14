241d5a3d3fe10ab550a892798a4e19980f9278bb
Ganesh20000 <ganeshkarli19@gmail.com>
2025-08-14 20:34:51 +0530

    revision

--- numpy_part2/phase_2.py
+++ numpy_part2/phase_2.py
@@ -15,6 +15,37 @@ print("with step",val[0:10:2])
 
 
 print("negative indexing", val[::-1])
+print("_"*50)
+
+matrix= np.array([[1,2,3],
+                  [4,5,6],
+                  [7,8,9]])
+
+print(matrix)
+
+print(arr)
+print("_"*50)
+#2d array specifi element
+print("specific no " ,arr[2,3])
+print("specifi no ",arr[2,2])
+print("specific no is ", arr[1,0])
+
+print("_"*50)
+
+print("specific no is ", matrix[1,1])
+print("specific no is ", matrix[2,2])
+
+print("_"*50)
+# entire row target 
 
 
-print
+print(arr[:,2])
+print(arr[:,3])
+print(matrix[:,0])
+
+print("sorting started")
+
+unsort = np.array([5,7,8,9,4,2,3,0,1,8,3,10])
+
+ars = np.sort(unsort)
+print(ars)
